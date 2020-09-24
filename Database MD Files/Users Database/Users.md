ae0560246cea2e8c80a77c401d9b4ebe6c7ea1a5
Brian Ruff <briananthonyruff@gmail.com>
2020-09-23 23:23:51 -0400

    dynamic table insertion feature added to test db

--- Database MD Files/Users Database/Users.md
+++ Database MD Files/Users Database/Users.md
@@ -1,3 +1,13 @@
-1. create database booty_bounter_users;
-2. create table users (username char(5), age integer);
-3. connection string: postgres://postgres:1366@localhost:5432/booty_bounter
+1. `create database booty_bounter_users;`
+2. `create table test (username char(5), age integer);`
+3. `connection string: postgres://postgres:1366@localhost:5432/booty_bounter`
+4. ```create table if not exists users
+      (id serial primary key,
+        username varchar(32),
+        password varchar(32),
+        dob date,
+        ow_real_id varchar(32),
+        reviewer boolean default false,
+        timestamp date default current_date
+      );
+   ```
--- index.js
+++ index.js
@@ -1,15 +1,50 @@
+require("dotenv").config();
 const postgres = require("postgres");
-const sql = postgres("postgres://postgres:1366@localhost:5432/booty_bounter");
+const sql = postgres(process.env.PSQL_STRING);
 
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
 
-const dataCall = async function () {
-  return await sql`select * from test;`;
-};
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+const data = async () => await sql`select * from users;`;
+
+app.post("/test", (req, res) => {
+  const user = req.body;
+  console.log(user);
+  try {
+    const newUser = async () =>
+      await sql`
+      insert into users (
+        username, password, dob, ow_real_id, reviewer, timestamp
+      )
+      values (
+        ${user.username},
+        ${user.password},
+        ${user.dob},
+        ${user.ow_real_id},
+        ${user.reviewer},
+        ${user.timestamp}
+      );
+    `;
+    newUser().then((r) => console.log("New User", r));
+  } catch (error) {
+    console.log("error", error);
+  }
+});
 
 app.get("/test", (req, res) => {
-  return dataCall().then((result) => res.json(result));
+  data().then((result) => {
+    console.log(result);
+    return res.json(result);
+  });
 });
 
 app.listen(4000, () => console.log("server running on port 4000"));
